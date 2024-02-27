f1b0a56506c61e6db8bc3ec9181fd51a58bda97a
MikeO <10thmountain@zoho.com>
2024-02-27 13:16:07 +0800

    Update walletDaemon.js

--- src/walletDaemon.js
+++ src/walletDaemon.js
@@ -86,7 +86,7 @@ module.exports = class walletNode extends EventEmitter {
 
   sendWithFee (recipient, amount, password, fee, changeAddrOverride) {
     return new Promise((resolve, reject) => {
-      this._client.Send({ toAddress: recipient, amount, password, from: [], fee, changeAddrOverride }, (err, data) => {
+      this._client.SendWithFee({ toAddress: recipient, amount, password, from: [], fee, changeAddrOverride }, (err, data) => {
         if (err !== null) return reject(err)
 
         resolve(data.txIDs)
